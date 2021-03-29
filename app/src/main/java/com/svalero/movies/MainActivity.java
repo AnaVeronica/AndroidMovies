b69712ff3a304b4a3186fc49242a3cde8883fb02
Verónica Pinos <veronica@MacBook-Pro-de-Veronica.local>
2021-03-29 15:42:10 +0200

    Se ha añadido un SplashScreen

--- app/src/main/java/com/svalero/movies/MainActivity.java
+++ app/src/main/java/com/svalero/movies/MainActivity.java
@@ -2,14 +2,33 @@ package com.svalero.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.svalero.movies.movies.lstMovies.view.LstMoviesActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SCREEN = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Lanzar un proceso en 2º plano que se active a los 3sg
+        final Handler handler = new Handler();
+        handler.postDelayed(
+                new Runnable() { // Interface
+                    @Override
+                    public void run() {
+                        // Cargar la 2ª pantalla
+                        Intent intent = new Intent(
+                                getBaseContext(), LstMoviesActivity.class);
+                        startActivity(intent);
+                    }
+                }
+                , 3000
+        );
     }
 }
